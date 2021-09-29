65f1ec2ee12810698225e0492ae7bbf497b4702f
Maia <felipe.m992@gmail.com>
2021-09-29 15:59:50 -0300

    .

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -1,30 +1,32 @@
 const WS_URL_CONN_INFO = '/api/v1/websocket/connection-info'
+const MSG_ALREADY_CONNECTED = 'J\u00e1 est\u00e1 conectado no websocket!'
 
 angular.module('darwinApp', []).controller('darwinController', ($scope, $http) => {
-
-  $scope.xuruvis = 'XURUUUUUUUUUUVIS'
 
   $scope.receivedMessageHandler = (msg) => console.log(msg)
   $scope.connectionErrorHandler = (msg) => console.error(msg)
 
-  $scope.connect = () => $http.get(WS_URL_CONN_INFO).then((response) => {
-    $scope.serverMessageChannel = response.data.serverMessageChannel
-    $scope.stompClient = Stomp.over(new SockJS(response.data.endpoint))
-    $scope.stompClient.connect({}, () => {
-      response.data.listeners.forEach((listener) => {
-        $scope.stompClient.subscribe(listener, (serverMessage) => {
-          $scope.receivedMessageHandler(JSON.parse(serverMessage.body))
-        }, $scope.connectionErrorHandler)
+  $scope.connect = () => { if(!$scope.stompClient) {
+    $http.get(WS_URL_CONN_INFO).then((response) => {
+      $scope.serverMessageChannel = response.data.serverMessageChannel
+      $scope.stompClient = Stomp.over(new SockJS(response.data.endpoint))
+      $scope.stompClient.connect({}, () => {
+        response.data.listeners.forEach((listener) => {
+          $scope.stompClient.subscribe(listener, (serverMessage) => {
+            $scope.receivedMessageHandler(JSON.parse(serverMessage.body))
+          }, $scope.connectionErrorHandler)
+        })
       })
-    })
-  }, (error) => {
-    $scope.connectionErrorHandler(`HTTP ${error.status}`)
-  })
+    }, (error) => {
+      $scope.connectionErrorHandler(`HTTP ${error.status}`)
+    }) } else { $scope.connectionErrorHandler(MSG_ALREADY_CONNECTED) }
+  }
   $scope.connect()
 
   $scope.disconnect = () => {
     $scope.stompClient.disconnect(() => $scope.connectionErrorHandler('Disconnecting...'))
     $scope.stompClient = null
+    $scope.serverMessageChannel = null
   }
 
   $scope.sendMessage = (msg) => {
@@ -33,5 +35,7 @@ angular.module('darwinApp', []).controller('darwinController', ($scope, $http) =
         JSON.stringify({ 'message': msg }))
     }
   }
+
+  $scope.xuruvis = 'XURUUUUUUUUUUVIS'
 
 })
