7f22a5db88e4d8a74b96646eed6d92a4e69bd448
Maia <felipe.m992@gmail.com>
2021-09-29 16:10:42 -0300

    .

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -1,7 +1,22 @@
 const WS_URL_CONN_INFO = '/api/v1/websocket/connection-info'
 const MSG_ALREADY_CONNECTED = 'J\u00e1 est\u00e1 conectado no websocket!'
+const MSG_PREFIX_ERROR_LOADING_CONN_INFO = 'Erro ao buscar informa\u00e7\u00f5es de conex\u00e3o'
+const MSG_DISCONNECTING = 'Desconectando...'
 
 angular.module('darwinApp', []).controller('darwinController', ($scope, $http) => {
+
+  $scope.sendMessage = (msg) => {
+    if(!!$scope.stompClient && !!$scope.serverMessageChannel) {
+      $scope.stompClient.send($scope.serverMessageChannel, {},
+        JSON.stringify({ 'message': msg }))
+    }
+  }
+
+  $scope.disconnect = () => {
+    $scope.stompClient.disconnect(() => $scope.connectionErrorHandler(MSG_DISCONNECTING))
+    $scope.stompClient = null
+    $scope.serverMessageChannel = null
+  }
 
   $scope.receivedMessageHandler = (msg) => console.log(msg)
   $scope.connectionErrorHandler = (msg) => console.error(msg)
@@ -16,25 +31,12 @@ angular.module('darwinApp', []).controller('darwinController', ($scope, $http) =
             $scope.receivedMessageHandler(JSON.parse(serverMessage.body))
           }, $scope.connectionErrorHandler)
         })
-      })
+      }, $scope.disconnect)
     }, (error) => {
-      $scope.connectionErrorHandler(`HTTP ${error.status}`)
+      $scope.connectionErrorHandler(`${MSG_PREFIX_ERROR_LOADING_CONN_INFO} (HTTP ${error.status})`)
     }) } else { $scope.connectionErrorHandler(MSG_ALREADY_CONNECTED) }
   }
   $scope.connect()
-
-  $scope.disconnect = () => {
-    $scope.stompClient.disconnect(() => $scope.connectionErrorHandler('Disconnecting...'))
-    $scope.stompClient = null
-    $scope.serverMessageChannel = null
-  }
-
-  $scope.sendMessage = (msg) => {
-    if(!!$scope.stompClient && !!$scope.serverMessageChannel) {
-      $scope.stompClient.send($scope.serverMessageChannel, {},
-        JSON.stringify({ 'message': msg }))
-    }
-  }
 
   $scope.xuruvis = 'XURUUUUUUUUUUVIS'
 
